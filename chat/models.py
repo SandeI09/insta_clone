d20e215455405b41126e65be1ffbf3ff3dec715c
Sandei <sandesh.subedi.10.09@gmail.com>
2023-08-21 19:28:57 +0545

    Chatbox and others

--- chat/models.py
+++ chat/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 # Create your models here.
 
 class Message(models.Model):
-    form_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="message_from")
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="message_from")
     to_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="message_to")
     text = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
--- chat/views.py
+++ chat/views.py
@@ -9,11 +9,16 @@ from django.http import HttpResponseRedirect
 
 User = get_user_model()
 
+def message(request):
+    users = User.objects.exclude(username=request.user.username)
+    context = {"users": users, "user_list": True}
+    return render(request, "chat.html", context)
+
 def user_chat(request, username):
     user = get_object_or_404(User, username = username)
     users = User.objects.exclude(username = request.user.username)
     messages = Message.objects.filter(
-        Q(form_user=user, to_user=request.user) | Q(form_user=request.user, to_user = user)
+        Q(from_user=user, to_user=request.user) | Q(from_user=request.user, to_user = user)
     )
     form = MessageForm(request.POST or None)
     if form.is_valid():

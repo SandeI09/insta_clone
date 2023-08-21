9dbcc13ae4a9475d791f83f233610a552e6fc773
Sandei <sandesh.subedi.10.09@gmail.com>
2023-08-21 16:37:36 +0545

    Chat box

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.contrib.auth import get_user_model
 from chat.models import Message
 from django.db.models import Q
+from chat.forms import MessageForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -13,6 +15,13 @@ def user_chat(request, username):
     messages = Message.objects.filter(
         Q(form_user=user, to_user=request.user) | Q(form_user=request.user, to_user = user)
     )
-    context = {"users": users, "messages": messages}
+    form = MessageForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.from_user = request.user
+        obj.to_user = user
+        obj.save()
+        return HttpResponseRedirect(reverse("chat:message", args=(username, )))
+    context = {"users": users, "messages": messages, "form": form}
 
     return render(request, "chat.html", context)
